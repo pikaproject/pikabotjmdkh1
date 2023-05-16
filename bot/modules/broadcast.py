3e82824390328c25db4211e9d4a851b84c3921ba
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-05-16 22:41:19 +0800

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -32,7 +32,7 @@ async def broadcast(client, message):
         msg += f"<b>Total {users_count} users in Database</b>\n"
         msg += f"<b>Sucess: </b>{success} users\n"
         msg += f"<b>Failed: </b>{users_count - success} users"
-        return sendMessage(msg, message.copy, update.message) 
+        return sendMessage(msg, message.copy, message) 
 
 bot.add_handler(MessageHandler(broadcast, filters=command(
     BotCommands.Broadcast) & CustomFilters.sudo))
