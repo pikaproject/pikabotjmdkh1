c285c11f5e4fba3e21b249554c3f780b5c99315a
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-05-16 23:18:05 +0800

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -7,11 +7,11 @@ from pyrogram.filters import command, regex
 from pyrogram.handlers import CallbackQueryHandler, MessageHandler
 from bot.helper.telegram_helper.bot_commands import BotCommands
 
-async def broadcast(update, context):
-    reply_to = update.message.reply_to_message
+async def broadcast(client, message):
+    reply_to = message.reply_to_message
 
     if not config_dict['DATABASE_URL']:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"DATABASE_URL not provided")
+        await message.reply_text("DATABASE_URL not provided")
     else:
         conn = MongoClient(config_dict['DATABASE_URL'])
         db = conn.mltb
@@ -23,7 +23,7 @@ async def broadcast(update, context):
         
         for chat_id in chat_ids:
             try:
-                context.bot.copy_message(chat_id=chat_id, from_chat_id=update.message.chat.id, message_id=reply_to.message_id)
+                await client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=reply_to.message_id)
                 success += 1
             except Exception as err:
                 LOGGER.error(err)
@@ -32,7 +32,7 @@ async def broadcast(update, context):
         msg += f"<b>Total {users_count} users in Database</b>\n"
         msg += f"<b>Sucess: </b>{success} users\n"
         msg += f"<b>Failed: </b>{users_count - success} users"
-        return sendMessage(msg, context.bot, update.message) 
+        return sendMessage(msg, client, message) 
 
 bot.add_handler(MessageHandler(broadcast, filters=command(
     BotCommands.Broadcast) & CustomFilters.sudo))
