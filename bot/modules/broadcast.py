53a073fd0dc2cf6ebf652e996df4692c8a8ee810
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-05-16 23:55:02 +0800

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 from bot import bot, LOGGER, config_dict
 from bot.helper.telegram_helper.filters import CustomFilters
-from pyrogram.filters import command, regex
-from pyrogram.handlers import CallbackQueryHandler, MessageHandler
+from pyrogram.filters import command
+from pyrogram.handlers import MessageHandler
 from bot.helper.telegram_helper.bot_commands import BotCommands
 
 async def sendMessage(client, chat_id, text):
@@ -13,32 +13,20 @@ async def sendMessage(client, chat_id, text):
         LOGGER.error(str(e))
         
 async def broadcast(client, message):
+    print("Broadcast function called")
     reply_to = message.reply_to_message
+    print("Reply message:", reply_to)
 
-    if not config_dict['DATABASE_URL']:
-        await message.reply_text("DATABASE_URL not provided")
-    else:
-        conn = MongoClient(config_dict['DATABASE_URL'])
-        db = conn.mltb
-        users_collection = db.users
-        users_count = db.users.count_documents({})
+    # ...
     
-        chat_ids = [str(user["_id"]) for user in users_collection.find({}, {"_id": 1})]
-        success = 0
-        
-        for chat_id in chat_ids:
-            try:
-                await client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=reply_to.message_id)
-                success += 1
-            except Exception as err:
-                LOGGER.error(err)
-
-        msg = f"<b>Broadcasting Completed</b>\n"
-        msg += f"<b>Total {users_count} users in Database</b>\n"
-        msg += f"<b>Sucess: </b>{success} users\n"
-        msg += f"<b>Failed: </b>{users_count - success} users"
-        for chat_id in chat_ids:
-            await sendMessage(client, chat_id, msg)
+    for chat_id in chat_ids:
+        print("Processing chat ID:", chat_id)
+        try:
+            # ...
+        except Exception as err:
+            LOGGER.error(err)
+    
+    # ...
 
 bot.add_handler(MessageHandler(broadcast, filters=command(
     BotCommands.Broadcast) & CustomFilters.sudo))
