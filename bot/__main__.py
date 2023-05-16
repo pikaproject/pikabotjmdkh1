02bd5b1a92b1c6c6cc633288388e2331da013100
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-05-16 21:47:43 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -32,7 +32,7 @@ from .modules import (anonymous, authorize, bot_settings, cancel_mirror,
                       category_select, clone, eval, gd_count, gd_delete,
                       gd_list, leech_del, mirror_leech, rmdb, rss,
                       save_message, shell, status, torrent_search,
-                      torrent_select, users_settings, ytdlp)
+                      torrent_select, users_settings, ytdlp, broadcast)
 
 start_aria2_listener()
 
