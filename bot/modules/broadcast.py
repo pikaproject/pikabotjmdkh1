2eb24b8e4d4d8ec0e48586324d64fe5e574f42c8
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-05-25 13:04:13 +0700

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -7,11 +7,11 @@ from pyrogram.filters import command
 from pyrogram.handlers import MessageHandler
 from bot.helper.telegram_helper.bot_commands import BotCommands
 
-async def broadcast(client, message):
+async def broadcast(bot, message):
     reply_to = message.reply_to_message
 
     if not config_dict['DATABASE_URL']:
-        await client.send_message(chat_id=message.chat.id, text="DATABASE_URL not provided")
+        await bot.send_message(chat_id=message.chat.id, text="DATABASE_URL not provided")
     else:
         conn = MongoClient(config_dict['DATABASE_URL'])
         db = conn.mltb
@@ -23,7 +23,7 @@ async def broadcast(client, message):
 
         for chat_id in chat_ids:
             try:
-                await client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=reply_to.message_id)
+                await bot.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=reply_to.message_id)
                 success += 1
             except Exception as err:
                 LOGGER.error(err)
@@ -32,6 +32,6 @@ async def broadcast(client, message):
         msg += f"<b>Total {users_count} users in Database</b>\n"
         msg += f"<b>Success: </b>{success} users\n"
         msg += f"<b>Failed: </b>{users_count - success} users"
-        await sendMessage(msg, client, message)
+        await sendMessage(msg, bot, message)
 
 bot.add_handler(MessageHandler(broadcast, filters=command(BotCommands.Broadcast) & CustomFilters.sudo))
