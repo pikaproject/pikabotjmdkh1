9cd7d500d426fe573126ab35e1a3771fff543dfa
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-05-29 09:33:17 +0700

    try Broadcast

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -1,18 +1,18 @@
 from pymongo import MongoClient
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
 from bot import bot, LOGGER, config_dict
-from bot.helper.telegram_helper.message_utils import sendMessage
 from bot.helper.telegram_helper.filters import CustomFilters
-from pyrogram.filters import command
-from pyrogram.handlers import MessageHandler
-from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.telegram_helper.message_utils import sendMessage
 
-async def broadcast(bot, message):
+
+@bot.on_message(filters.command('broadcast1') & CustomFilters.owner_filter)
+def broadcast(client, message):
     reply_to = message.reply_to_message
 
     if not config_dict['DATABASE_URL']:
-        limz = "Sorry DATABASE_URL not Provided, Please Check and add Your Config"
-        await sendMessage(limz, bot, message)
+        client.send_message(chat_id=message.chat.id, text=f"DATABASE_URL not provided")
     else:
         conn = MongoClient(config_dict['DATABASE_URL'])
         db = conn.mltb
@@ -24,7 +24,7 @@ async def broadcast(bot, message):
 
         for chat_id in chat_ids:
             try:
-                await bot.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=reply_to.message_id)
+                client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=reply_to.message_id)
                 success += 1
             except Exception as err:
                 LOGGER.error(err)
@@ -33,6 +33,5 @@ async def broadcast(bot, message):
         msg += f"<b>Total {users_count} users in Database</b>\n"
         msg += f"<b>Success: </b>{success} users\n"
         msg += f"<b>Failed: </b>{users_count - success} users"
-        return await sendMessage(msg, bot, message)
+        sendMessage(msg, client, message)
 
-bot.add_handler(MessageHandler(broadcast, filters=command(BotCommands.Broadcast) & CustomFilters.sudo))
