adfde67553e49ee1a574b62459463b4b9ce3616e
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-05-30 21:01:25 +0700

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -3,6 +3,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from pyrogram.handlers import MessageHandler
+from pyrogram.filters import command
 from bot import bot, config_dict, LOGGER
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage
