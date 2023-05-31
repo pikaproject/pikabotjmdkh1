20bbe637c42dfd804f343cfcdcb12b5806c0e708
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-05-31 18:02:35 +0700

    update bot/modules/broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -34,7 +34,7 @@ async def broadcast(client, message):
         msg += f"<b>Total {users_count} users in Database</b>\n"
         msg += f"<b>Sucess: </b>{success} users\n"
         msg += f"<b>Failed: </b>{users_count - success} users"
-        await message.reply(message, msg)
+        await message.reply(client, message, msg)
 
 
 bot.add_handler(MessageHandler(broadcast, filters=command(BotCommands.Broadcast) & CustomFilters.sudo))
