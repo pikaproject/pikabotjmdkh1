90b0e16a26319e28c17bbee8e4c0efc7eec39cb9
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-05-31 11:22:09 +0700

    update bot/modules/broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -24,8 +24,8 @@ async def broadcast(client, message):
 
         for chat_id in chat_ids:
             try:
-                reply_to_id = message.reply_to_message.message_id
-                await client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=reply_to_id)
+                #reply_to_id = message.reply_to_message.message_id
+                await client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, reply_to_message_id=message.id)
                 success += 1
             except Exception as err:
                 LOGGER.error(err)
