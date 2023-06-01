307e1d179c1b51bda27a09042c559099d72309ad
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-06-01 11:32:55 +0700

    update bot/modules/broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -28,7 +28,8 @@ async def broadcast(client, message : Message):
 
         for chat_id in chat_ids:
             try:
-                await client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=message.id)
+                ids = chat_id.split(" ", 1)
+                await client.copy_message(chat_id=ids, from_chat_id=message.chat.id, message_id=message.id)
                 success += 1
             except Exception as err:
                 LOGGER.error(err)
@@ -37,7 +38,7 @@ async def broadcast(client, message : Message):
         msg += f"Total {users_count} users in Database\n"
         msg += f"Sucess: {success} users\n"
         msg += f"Failed: {users_count - success} users"
-        await editMessage(a, msg)
+        await message.reply(msg, message)
 
 
 bot.add_handler(MessageHandler(broadcast, filters=command(BotCommands.Broadcast) & CustomFilters.sudo))
