a5b972c56e72a8f480ef158e62701d089963fc97
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-06-01 21:44:55 +0700

    update bot/modules/broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -12,8 +12,8 @@ from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
 
 async def broadcast(client, message):
     #replied = message.reply_to_message
-    #limz = "Broadcast your Message Please wait...."
-    #a = await message.reply(client, message, limz)
+    limz = "Broadcast your Message Please wait...."
+    a = await message.reply(client, message, limz)
     
     if not config_dict['DATABASE_URL']:
         await client.send_message(chat_id=message.chat.id, text=f"DATABASE_URL not provided")
@@ -37,7 +37,7 @@ async def broadcast(client, message):
         msg += f"Total {users_count} users in Database\n"
         msg += f"Sucess: {success} users\n"
         msg += f"Failed: {users_count - success} users"
-        await message.edit(msg, message)
+        await message.edit(msg, a)
 
 async def broadcast_psn(client, message):
     mess = message.text.split()
