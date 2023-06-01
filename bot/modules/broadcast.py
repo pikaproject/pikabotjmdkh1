3d4cad6338474800330b2cbd12f978eed04c0992
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-06-01 10:55:31 +0700

    update bot/modules/broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -13,7 +13,7 @@ from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
 async def broadcast(client, message : Message):
     replied = message.reply_to_message
     limz = "Broadcast your Message Please wait...."
-    a = sendMessage(client, message, limz)
+    a = await sendMessage(client, message, limz)
     
     if not config_dict['DATABASE_URL']:
         await client.send_message(chat_id=message.chat.id, text=f"DATABASE_URL not provided")
@@ -28,7 +28,7 @@ async def broadcast(client, message : Message):
 
         for chat_id in chat_ids:
             try:
-                await client.copy_message(chat_id=chat_id, from_chat_id=message.chat.id, message_id=message.id)
+                await client.copy_message(chat_id=chat_id, from_chat_id=message.from_user.id, message_id=message.id)
                 success += 1
             except Exception as err:
                 LOGGER.error(err)
@@ -37,7 +37,7 @@ async def broadcast(client, message : Message):
         msg += f"Total {users_count} users in Database\n"
         msg += f"Sucess: {success} users\n"
         msg += f"Failed: {users_count - success} users"
-        await editMessage(msg, a)
+        await editMessage(a)
 
 
 bot.add_handler(MessageHandler(broadcast, filters=command(BotCommands.Broadcast) & CustomFilters.sudo))
