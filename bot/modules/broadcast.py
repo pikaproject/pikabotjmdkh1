b73c8da35325a89f3d50108e020173adc58d2ed7
Raysha7 <79763285+Raysha7@users.noreply.github.com>
2023-06-12 08:52:21 +0700

    update bot/modules/broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -14,18 +14,15 @@ from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
 async def broadcast(bot, message):
     mess = message.text
     replied = message.reply_to_message
-    if (replied == 0 ) and len(mess) == 1 :
+    if len(mess) == 1 and replied is None :
         mess = mess.split(maxsplit=1)[1]
-        success = 0
         totals = len(user_data)
         for chat_id in user_data:
-            reply_to=message.reply_to_message
             try:
                 await bot.send_message(chat_id=chat_id, text= mess)
             except Exception as e:
                LOGGER.error(e)
                continue
-            success += 1
         msg = f"Broadcasting Completed ✅\n"
         msg += f"Total {totals} users in Database\n"
         await message.reply(msg, message)
